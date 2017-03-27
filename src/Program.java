2e34de28d4348a83b149d57c95d7a6d2f5b1affe
Alan Tse <starttodevelop@gmail.com>
2017-03-27 15:37:19 +1100

    JFrame

--- src/Program.java
+++ src/Program.java
@@ -3,7 +3,8 @@ public class Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Testing Git");
+		ExampleFrame frame = new ExampleFrame();
+		frame.setVisible(true);
 	}
 
 }
